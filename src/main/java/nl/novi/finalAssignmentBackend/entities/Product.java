e32db8e06af84a66b9abb3b40ac5e700766d3986
MelroyL92 <melroy_lingsma@live.nl>
2024-03-11 11:52:40 +0100

    Made the basis of the productClass

---
+++ src/main/java/nl/novi/finalAssignmentBackend/entities/Product.java
@@ -0,0 +1,90 @@
+package nl.novi.finalAssignmentBackend.entities;
+
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "Products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private Integer price;
+
+    private Integer originalStock;
+
+    private String description;
+
+    private String name;
+
+    private Integer amountSold;
+
+
+
+    public Product() {
+
+    }
+
+
+    public Product(Long id, Integer price, Integer originalStock, String description, String name, Integer amountSold){
+        this.id = id;
+        this.price = price;
+        this.originalStock = originalStock;
+        this.description = description;
+        this.name = name;
+        this.amountSold = amountSold;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getOriginalStock() {
+        return originalStock;
+    }
+
+    public void setOriginalStock(Integer originalStock) {
+        this.originalStock = originalStock;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAmountSold() {
+        return amountSold;
+    }
+
+    public void setAmountSold(Integer amountSold) {
+        this.amountSold = amountSold;
+    }
+}
