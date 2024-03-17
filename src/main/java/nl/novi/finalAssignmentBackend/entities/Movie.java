9bd803dddeb15568ac76465e7c83394404868b7e
MelroyL92 <melroy_lingsma@live.nl>
2024-03-17 13:50:38 +0100

    start of the model and entity for the movie and game instances

---
+++ src/main/java/nl/novi/finalAssignmentBackend/entities/Movie.java
@@ -0,0 +1,61 @@
+package nl.novi.finalAssignmentBackend.entities;
+import jakarta.persistence.*;
+import nl.novi.finalAssignmentBackend.model.ProductModel;
+
+
+
+@Entity
+@Table(name = "Movies")
+public class Movie extends Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String genre;
+    private String type;
+    private String director;
+
+    private String playtime;
+
+
+    public String getPlaytime() {
+        return playtime;
+    }
+
+    public void setPlaytime(String playtime) {
+        this.playtime = playtime;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
---
+++ src/main/java/nl/novi/finalAssignmentBackend/model/MovieModel.java
@@ -0,0 +1,58 @@
+package nl.novi.finalAssignmentBackend.model;
+
+
+
+    public class MovieModel extends ProductModel {
+
+
+        private Long id;
+
+        private String genre;
+        private String type;
+        private String director;
+
+        private String playtime;
+
+        public String getPlaytime() {
+            return playtime;
+        }
+
+        public void setPlaytime(String playtime) {
+            this.playtime = playtime;
+        }
+
+        public String getGenre() {
+            return genre;
+        }
+
+        public void setGenre(String genre) {
+            this.genre = genre;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public String getDirector() {
+            return director;
+        }
+
+        public void setDirector(String director) {
+            this.director = director;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+}
+
+
