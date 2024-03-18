0ce5b4273a49164f8f77b12ec01a66c0f3bd0abe
MelroyL92 <melroy_lingsma@live.nl>
2024-03-18 10:49:39 +0100

    Added the user basics

---
+++ src/main/java/nl/novi/finalAssignmentBackend/entities/User.java
@@ -0,0 +1,38 @@
+package nl.novi.finalAssignmentBackend.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+
+    @Column(name = "Birth_Year")
+    private String BirthYear;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
