2b6ad9fbdf3bcd514255bc5eaa9dca5d8dbbecb0
Melroy <stargate_rules_forever@live.nl>
2024-04-01 12:06:11 +0200

    Startup Security

---
+++ src/main/java/nl/novi/finalAssignmentBackend/controllers/ExceptionController.java
@@ -0,0 +1,16 @@
+package nl.novi.finalAssignmentBackend.controllers;
+
+
+@ControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler(value = RecordNotFoundException.class)
+    public ResponseEntity<Object> exception(RecordNotFoundException exception) {
+
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+
+    }
+
+    /*TODO maak exceptionHandlers voor de 2 nieuwe exceptions*/
+
+}
