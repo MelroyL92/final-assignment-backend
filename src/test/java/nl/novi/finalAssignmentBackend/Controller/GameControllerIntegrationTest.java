36223acdcae851f52349e8b9bcaf9594071648f4
MelroyL92 <melroy_lingsma@live.nl>
2024-04-24 07:16:09 +0200

    Made a succesfull Controller integrationtest after solving one billion errors + removed duplicate dependencies from the POM.XML file

--- src/test/java/nl/novi/finalAssignmentBackend/Controller/GameControllerIntegrationTest.java
+++ src/test/java/nl/novi/finalAssignmentBackend/Controller/GameControllerIntegrationTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -37,9 +36,9 @@ public class GameControllerIntegrationTest {
                          "platform": "PC",
                          "publisher": "EA Games",
                          "playDurationInMin": 300,
-                         "amountSold: 10,
-                         "currentStock: 100,
-                         "purchasePrice: 10
+                         "amountSold": 10,
+                         "currentStock": 100,
+                         "purchasePrice": 10
           
                 }
                 """;
