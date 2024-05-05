2c1173a23c87f29e459811abc6e831d6adbe2fd3
MelroyL92 <melroy_lingsma@live.nl>
2024-05-05 13:18:40 +0200

    adjusted the exception of the user to be more descriptive of the error

--- src/main/java/nl/novi/finalAssignmentBackend/helper/LoggedInCheck.java
+++ src/main/java/nl/novi/finalAssignmentBackend/helper/LoggedInCheck.java
@@ -1,6 +1,7 @@
 package nl.novi.finalAssignmentBackend.helper;
 
-import jakarta.persistence.EntityNotFoundException;
+import nl.novi.finalAssignmentBackend.exceptions.NoUserAssignedException;
+import nl.novi.finalAssignmentBackend.exceptions.UserMismatchException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -13,13 +14,13 @@ public class LoggedInCheck {
             String loggedInUsername = authentication.getName();
 
             if (!username.equals(loggedInUsername)) {
-                throw new EntityNotFoundException("Mismatch in usernames: " + username + " does not match the logged-in user.");
+                throw new UserMismatchException(username);
             }
         }
 
          public void verifyOwnerAuthorization(String expectedOwnerUsername, String loggedInUsername, String entityType) {
         if (!expectedOwnerUsername.equals(loggedInUsername)) {
-            throw new EntityNotFoundException("You are not authorized to perform operations on this " + entityType + ".");
+            throw new NoUserAssignedException("You are not authorized to perform operations on this " + entityType + ".");
             }
     }
 
