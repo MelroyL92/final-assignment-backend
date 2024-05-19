842021b6070c79ff311a6988d2029cd196a61bb7
MelroyL92 <melroy_lingsma@live.nl>
2024-05-19 21:05:19 +0200

    Enum helper

---
+++ src/main/java/nl/novi/finalAssignmentBackend/helper/enums.java
@@ -0,0 +1,16 @@
+package nl.novi.finalAssignmentBackend.helper;
+
+public class enums {
+
+    public enum ShoppingListType {
+        WISHLIST,
+        SHOPPING_LIST
+    }
+
+    public enum OrderStatus {
+        PENDING_PAYMENT,
+        PENDING_DELIVERY,
+        CANCELLED,
+        COMPLETE
+    }
+}
